de53c0a4450c478beef1b50afb345f17396d20e9
Dhairya Kataria <dhairya.kataria@somaiya.edu>
2024-02-17 12:10:36 +0530

    handled any amount of simple str with numbers

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ class StringCalculator:
     def add_str(self, str_numbers: str) -> int:
         if not str_numbers:
             return 0
+        
+        delimiter = ","
+
+        # Split the numbers using delimiter or newline
+        numbers_list = str_numbers.split(",")
+        
+        # Remove the extra white spaces before and after the number if any
+        numbers_list = [num.strip() for num in numbers_list]
+        
+        # Convert numbers to integers and filter out empty strings
+        numbers_list = [int(num) for num in numbers_list if num]
+        return sum(numbers_list)
--- main_test.py
+++ main_test.py
@@ -10,5 +10,16 @@ class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(self.calc.add_str(""), 0)
 
+    # Test single number
+    def test_single_number(self):
+        self.assertEqual(self.calc.add_str("1"), 1)
+
+    # Test multiple numbers separated by commas
+    def test_multiple_numbers_commas(self):
+        self.assertEqual(self.calc.add_str("1,5"), 6)
+
+    def test_multiple_numbers_commas_2(self):
+        self.assertEqual(self.calc.add_str("1, 5 ,  "), 6)
+
 if __name__ == '__main__':
     unittest.main()
